4ea87183fe039047d06f26f95775ada6f71f0e68
Olga <Taylakova_Olga@mail.ru>
2023-10-04 10:04:38 +0700

    add extra tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,22 +1,22 @@
 package ru.netology.statistic;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
 
 
-        @Test
-        public void findMax() {
-            StatisticsService service = new StatisticsService();
+    @Test
+    public void findMax() {
+        StatisticsService service = new StatisticsService();
 
-            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-            long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-            long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-            Assertions.assertEquals(expected, actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
     public void findMaxFromMiddle() {
